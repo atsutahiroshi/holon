c515c666cbe47f208c9841ec16b90f8c8f88ecf2
Hiroshi Atsuta <hiroshi.atsuta@ams.eng.osaka-u.ac.jp>
2018-02-05 11:48:25 +0100

    test(humanoid): rewrite test code in table-driven style

--- holon/corelib/humanoid/com_ctrl_test.cpp
+++ holon/corelib/humanoid/com_ctrl_test.cpp
@@ -105,14 +105,18 @@ TEST_CASE("compute desired ZMP position from the referential COM position",
 
   SECTION("referential COM position is set as (0, 0, 1)") {
     zVec3D ref_com_pos = {0, 0, 1};
-    zVec3D com_pos = {0, 0, 1};
-    zVec3D com_vel = {0, 0, 0};
-    zVec3D desired_zmp_pos;
-    zVec3D expected_zmp_pos = {0, 0, 0};
+    struct testcase_t {
+      zVec3D com_pos;
+      zVec3D com_vel;
+      zVec3D expected_zmp_pos;
+    } testcases[] = {{{0, 0, 1}, {0, 0, 0}, {0, 0, 0}}};
 
-    ctrl.ComputeDesiredZmpPosition(&ref_com_pos, &com_pos, &com_vel,
-                                   &desired_zmp_pos);
-    CHECK_THAT(&desired_zmp_pos, Catch::Equals(&expected_zmp_pos));
+    for (auto& c : testcases) {
+      zVec3D desired_zmp_pos;
+      ctrl.ComputeDesiredZmpPosition(&ref_com_pos, &c.com_pos, &c.com_vel,
+                                     &desired_zmp_pos);
+      CHECK_THAT(&desired_zmp_pos, Catch::Equals(&c.expected_zmp_pos));
+    }
   }
 }
 

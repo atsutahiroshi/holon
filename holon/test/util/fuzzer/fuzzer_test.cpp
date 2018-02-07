7bcf530ab791d63fa61a2678bfab0925523b0702
Hiroshi Atsuta <hiroshi.atsuta@ams.eng.osaka-u.ac.jp>
2018-02-07 18:35:43 +0100

    test(util): add test for Fuzzer class

---
+++ holon/test/util/fuzzer/fuzzer_test.cpp
@@ -0,0 +1,42 @@
+/* fuzzer - simple fuzzer
+ *
+ * Copyright (c) 2018 Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
+ *
+ * This file is part of the holon.
+ *
+ * The holon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The holon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the holon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "holon/test/util/fuzzer/fuzzer.hpp"
+
+#include <vector>
+
+#include "catch.hpp"
+
+namespace holon {
+namespace {
+
+TEST_CASE("outputs random values", "[test][util][fuzzer]") {
+  std::vector<double> outdated_values;
+  Fuzzer fuzz;
+
+  for (auto i = 0; i < 10; ++i) {
+    double n = fuzz.get();
+    REQUIRE_THAT(outdated_values, !Catch::Matchers::VectorContains(n));
+    outdated_values.push_back(n);
+  }
+}
+
+}  // namespace
+}  // namespace holon

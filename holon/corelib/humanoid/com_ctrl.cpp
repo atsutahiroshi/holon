b5ab892f506be4fa9269301ed6f23fa5cee28c8f
Hiroshi Atsuta <hiroshi.atsuta@ams.eng.osaka-u.ac.jp>
2018-02-07 14:49:56 +0100

    refactor(humanoid): code cleanup

--- holon/corelib/humanoid/com_ctrl.cpp
+++ holon/corelib/humanoid/com_ctrl.cpp
@@ -34,21 +34,16 @@ double ComCtrl::computeDesiredZeta(const zVec3D* ref_com_position) const {
   return m_model.computeZeta(ref_com_position);
 }
 
-zVec3D* ComCtrl::computeDesiredZmpPosition(const zVec3D* ref_com_position,
-                                           const zVec3D* com_position,
-                                           const zVec3D* com_velocity,
-                                           zVec3D* desired_zmp_position) const {
-  double xd = zVec3DElem(ref_com_position, zX);
-  double x = zVec3DElem(com_position, zX);
-  double vx = zVec3DElem(com_velocity, zX);
-  double yd = zVec3DElem(ref_com_position, zY);
-  double y = zVec3DElem(com_position, zY);
-  double vy = zVec3DElem(com_velocity, zY);
-  double zeta = computeDesiredZeta(ref_com_position);
-  zVec3DCreate(desired_zmp_position,
-               m_x.computeDesiredZmpPosition(xd, x, vx, zeta),
-               m_y.computeDesiredZmpPosition(yd, y, vy, zeta), 0);
-  return desired_zmp_position;
+zVec3D* ComCtrl::computeDesiredZmpPosition(const zVec3D* ref_com_pos,
+                                           const zVec3D* com_pos,
+                                           const zVec3D* com_vel,
+                                           zVec3D* desired_zmp_pos) const {
+  double zeta = computeDesiredZeta(ref_com_pos);
+  zVec3DCreate(
+      desired_zmp_pos,
+      m_x.computeDesiredZmpPosition(ref_com_pos, com_pos, com_vel, zeta),
+      m_y.computeDesiredZmpPosition(ref_com_pos, com_pos, com_vel, zeta), 0);
+  return desired_zmp_pos;
 }
 
 }  // namespace holon

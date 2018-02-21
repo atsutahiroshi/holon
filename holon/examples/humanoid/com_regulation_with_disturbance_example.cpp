948600077ba9c2b806b6fdbcb1702b2d17a15915
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-02-21 19:31:03 +0100

    feat(humanoid): add example for ComCtrl

---
+++ holon/examples/humanoid/com_regulation_with_disturbance_example.cpp
@@ -0,0 +1,78 @@
+/* com_regulation_with_disturbance_example - example of COM regulator with
+ * disturbance
+ *
+ * Copyright (c) 2018 Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
+ *
+ * This file is part of the holon.
+ *
+ * The holon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The holon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the holon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include "holon/corelib/humanoid/com_ctrl.hpp"
+
+const double T = 10;
+const double DT = 0.01;
+
+using holon::Vec3D;
+using holon::ComZmpModel;
+using holon::ComCtrl;
+
+void apply_external_force(const ComZmpModel& model, double t) {
+  Vec3D force1 = {1, -1, 0};
+  Vec3D force2 = {-1.5, 1.5, 0};
+
+  if (t > 4 && t < 4.1) {
+    model.data_ptr()->external_force = force1;
+  } else if (t > 6 && t < 6.1) {
+    model.data_ptr()->external_force = force2;
+  } else {
+    model.data_ptr()->external_force = holon::kVec3DZero;
+  }
+}
+
+int main() {
+  ComZmpModel model;
+  model.reset(Vec3D(0.1, -0.1, 1));
+
+  // ComCtrl ctrl(model);
+  ComCtrl ctrl;
+  ctrl.states().com_position = model.data().com_position;
+  auto cmd = ctrl.getUserCommands();
+  Vec3D cmd_com_pos = {0, 0, 1};
+
+  double t = 0;
+  while (t < T) {
+    // feedback
+    ctrl.states().com_position = model.data().com_position;
+    ctrl.states().com_velocity = model.data().com_velocity;
+
+    // update controller
+    cmd->set_com_position(cmd_com_pos);
+    ctrl.update(DT);
+
+    // update simulator
+    model.data_ptr()->zmp_position = ctrl.outputs().zmp_position;
+    apply_external_force(model, t);
+    model.update(DT);
+
+    // logging
+    std::cout << t << " ";
+    std::cout << model.data().com_position.data() << " ";
+    std::cout << model.data().com_velocity.data() << " ";
+    std::cout << model.data().zmp_position.data() << "\n";
+    t += DT;
+  }
+  return 0;
+}
---
+++ holon/examples/humanoid/tests/com_regulation_with_disturbance_example_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from subprocess import Popen
+import os
+import unittest
+
+
+SAMPLE_DATA_FILENAME = "data/com_regulation_with_disturbance_example.dat"
+TESTING_DATA_FILENAME = "result.dat"
+TEST_EXECUTABLE = "../com_regulation_with_disturbance_example"
+
+
+def load_sample_data():
+    data = np.loadtxt(SAMPLE_DATA_FILENAME)
+    return data
+
+
+def generate_testing_data():
+    cmd = [TEST_EXECUTABLE]
+    with open(TESTING_DATA_FILENAME, 'w') as fobj:
+        proc = Popen(cmd, stdout=fobj)
+        proc.wait(timeout=1)
+    data = np.loadtxt(TESTING_DATA_FILENAME)
+    return data
+
+
+class TestComRegulationWithDisturbance(unittest.TestCase):
+
+    def test_result(self):
+        # check if sample data exists
+        self.assertTrue(os.path.isfile(SAMPLE_DATA_FILENAME),
+                        msg="sample data does not exist")
+
+        # check if excutable exists
+        self.assertTrue(os.path.isfile(TEST_EXECUTABLE),
+                        msg="test executable does not exist")
+        self.assertTrue(os.access(TEST_EXECUTABLE, os.X_OK),
+                        msg="test executable is not executable")
+
+        # check if generated data matches sample data
+        sample = load_sample_data()
+        testing = generate_testing_data()
+        self.assertTrue(np.allclose(sample, testing),
+                        msg="sample and testing data do not match")
+
+
+if __name__ == '__main__':
+    unittest.main()

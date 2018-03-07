c4d418f12504492236cd7f44f819737d1a462db5
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-03-07 19:34:39 +0100

    feat(humanoid): add PointMassModelSystem class

---
+++ holon/corelib/humanoid/point_mass_model/point_mass_model_system.hpp
@@ -0,0 +1,116 @@
+/* point_mass_model_system - System definition of point mass model
+ *
+ * Copyright (c) 2018 Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
+ *
+ * This file is part of the holon.
+ *
+ * The holon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The holon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the holon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HOLON_HUMANOID_POINT_MASS_MODEL_SYSTEM_HPP_
+#define HOLON_HUMANOID_POINT_MASS_MODEL_SYSTEM_HPP_
+
+#include <array>
+#include <functional>
+#include "holon/corelib/humanoid/point_mass_model/point_mass_model_data.hpp"
+
+namespace holon {
+
+template <typename StateType, typename DataType>
+class SystemBase {
+ protected:
+  using Self = SystemBase<StateType, DataType>;
+  using State = StateType;
+  using StateArray = std::array<State, 2>;
+  using Data = DataType;
+  using DataPtr = std::shared_ptr<DataType>;
+
+ public:
+  SystemBase(DataPtr t_data_ptr) : m_data_ptr(t_data_ptr) {}
+
+  // virtual function
+  virtual StateArray operator()(const StateArray& state,
+                                const double t) const = 0;
+
+  // accessors
+  DataPtr data_ptr() const noexcept { return m_data_ptr; }
+  Data data() const noexcept { return *m_data_ptr; }
+
+  // mutators
+  Self& set_data_ptr(DataPtr t_data_ptr) {
+    m_data_ptr = t_data_ptr;
+    return *this;
+  }
+
+ private:
+  DataPtr m_data_ptr;
+};
+
+template <typename State>
+class PointMassModelSystem
+    : public SystemBase<State, PointMassModelData<State>> {
+  using Base = SystemBase<State, PointMassModelData<State>>;
+  using StateArray = typename Base::StateArray;
+  using Data = typename Base::Data;
+  using DataPtr = typename Base::DataPtr;
+  using Self = PointMassModelSystem<State>;
+
+ public:
+  using Function =
+      std::function<State(const State&, const State&, const double)>;
+
+ public:
+  explicit PointMassModelSystem(DataPtr t_data_ptr) : Base(t_data_ptr) {}
+
+  StateArray operator()(const StateArray& state, const double t) const {
+    StateArray dxdt;
+    dxdt[0] = state[1];
+    dxdt[1] = acceleration(state[0], state[1], t);
+    return dxdt;
+  }
+
+  State acceleration(const State& p, const State& v, const double t) const {
+    if (f_acceleration) return f_acceleration(p, v, t);
+    return force(p, v, t) / this->data().mass;
+  }
+
+  State force(const State& p, const State& v, const double t) const {
+    if (!f_force) return State(0.0);
+    return f_force(p, v, t);
+  }
+
+  Self& set_acceleration(Function t_acceleration) {
+    f_acceleration = t_acceleration;
+    return *this;
+  }
+
+  Self& set_force(Function t_force) {
+    f_force = t_force;
+    return *this;
+  }
+
+ private:
+  Function f_acceleration = nullptr;
+  Function f_force = nullptr;
+};
+
+template <typename State>
+PointMassModelSystem<State> makePointMassModelSystem(
+    PointMassModelDataPtr<State> t_data_ptr) {
+  return PointMassModelSystem<State>(t_data_ptr);
+}
+
+}  // namespace holon
+
+#endif  // HOLON_HUMANOID_POINT_MASS_MODEL_SYSTEM_HPP_
---
+++ holon/corelib/humanoid/point_mass_model/point_mass_model_system_test.cpp
@@ -0,0 +1,125 @@
+/* point_mass_model_system - System definition of point mass model
+ *
+ * Copyright (c) 2018 Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
+ *
+ * This file is part of the holon.
+ *
+ * The holon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The holon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the holon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "holon/corelib/humanoid/point_mass_model/point_mass_model_system.hpp"
+
+#include "catch.hpp"
+#include "holon/test/util/fuzzer/fuzzer.hpp"
+
+namespace holon {
+namespace {
+
+template <typename T>
+using System = PointMassModelSystem<T>;
+
+template <typename T>
+System<T> prepareSystem(double mass, const T& v) {
+  auto data = createPointMassModelData(mass, v);
+  auto sys = makePointMassModelSystem(data);
+  return sys;
+}
+
+TEST_CASE("Constructors of PointMassModelSystem",
+          "[PointMassModelSystem][ctor]") {
+  auto data = createPointMassModelData(1.0, kVec3DZ);
+  auto sys = makePointMassModelSystem(data);
+  REQUIRE(sys.data_ptr() == data);
+}
+
+TEST_CASE("Accessors / mutators of PointMassModelSystem",
+          "[PointMassModelSystem][accessor][mutator]") {
+  auto data = createPointMassModelData(1.0, kVec3DZ);
+  auto sys = makePointMassModelSystem(data);
+
+  SECTION("data reference") {
+    CHECK(sys.data().mass == 1.0);
+    // assignment via data() is not allowed
+    // sys.data().mass = 2.0;
+  }
+
+  SECTION("data pointer") {
+    auto data2 = createPointMassModelData(2.0, kVec3DZero);
+    sys.set_data_ptr(data2);
+    REQUIRE(sys.data_ptr() == data2);
+  }
+}
+
+TEST_CASE("operator() of PointMassModelSystem", "[PointMassModelSystem][.]") {}
+
+template <typename T>
+void CheckAcceleration() {
+  Fuzzer fuzz(0, 10);
+  auto data = createPointMassModelData<T>(fuzz(), fuzz.get<T>());
+  auto sys = makePointMassModelSystem(data);
+  T p, v;
+  double t;
+  SECTION("if not set acceleration function, returns 0") {
+    CHECK(sys.acceleration(p, v, t) == T(0.0));
+  }
+  SECTION("if set acceleration function, returns computed acceleration") {
+    auto acc = fuzz.get<T>();
+    auto f_acc = [acc](const T&, const T&, const double) { return acc; };
+    sys.set_acceleration(f_acc);
+    CHECK(sys.acceleration(p, v, t) == acc);
+  }
+  SECTION("you can use data as well") {
+    auto f_acc = [sys](const T&, const T&, const double) {
+      return sys.data().mass * T(1.0);
+    };
+    sys.set_acceleration(f_acc);
+    CHECK(sys.acceleration(p, v, t) == data->mass * T(1.0));
+    data->mass = 3.0;
+    CHECK(sys.acceleration(p, v, t) == 3.0 * T(1.0));
+  }
+}
+
+TEST_CASE("PointMassModelSystem::acceleration", "[PointMassModelSystem]") {
+  SECTION("State type is double") { CheckAcceleration<double>(); }
+  SECTION("State type is Vec3D") { CheckAcceleration<Vec3D>(); }
+}
+
+template <typename T>
+void CheckForce() {
+  Fuzzer fuzz(0, 10);
+  auto mass = 2.0;
+  auto data = createPointMassModelData<T>(mass, fuzz.get<T>());
+  auto sys = makePointMassModelSystem(data);
+  T p, v;
+  double t;
+  SECTION("if not set force function, returns 0") {
+    CHECK(sys.force(p, v, t) == T(0.0));
+    CHECK(sys.acceleration(p, v, t) == T(0.0));
+  }
+  SECTION("if set force function, returns computed force") {
+    auto force = fuzz.get<T>();
+    auto f_force = [force](const T&, const T&, const double) { return force; };
+    sys.set_force(f_force);
+    CHECK(sys.force(p, v, t) == force);
+    CHECK(sys.acceleration(p, v, t) == force / data->mass);
+  }
+}
+
+TEST_CASE("PointMassModelSystem::force", "[PointMassModelSystem]") {
+  SECTION("State type is double") { CheckForce<double>(); }
+  SECTION("State type is Vec3D") { CheckForce<Vec3D>(); }
+}
+
+}  // namespace
+}  // namespace holon

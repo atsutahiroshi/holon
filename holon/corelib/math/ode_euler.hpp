47c0681be4270ec8e2785c88ac09b311f91bbc20
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-03-09 16:33:52 +0100

    feat(math): add ordinary differential equation solver

---
+++ holon/corelib/math/ode_euler.hpp
@@ -0,0 +1,74 @@
+/* ode_euler - ordinary differential equation quadrature: Euler method
+ *
+ * Copyright (c) 2018 Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
+ *
+ * This file is part of the holon.
+ *
+ * The holon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The holon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the holon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HOLON_MATH_ODE_EULER_HPP_
+#define HOLON_MATH_ODE_EULER_HPP_
+
+#include "holon/corelib/common/zip.hpp"
+
+namespace holon {
+
+template <typename Solver>
+class OdeSolver {
+ public:
+  template <typename System, typename State, typename Time>
+  State update(const System& system, const State& x, const Time t,
+               const Time dt) {
+    return this->solver().update_impl(system, x, t, dt);
+  }
+
+ private:
+  Solver& solver() { return *static_cast<Solver*>(this); }
+};
+
+namespace operations {
+
+template <typename State, typename Time>
+State cat(const State& state, const Time dt, const State& deriv) {
+  State state_out;
+  auto x = state.begin();
+  auto dxdt = deriv.begin();
+  auto x_out = state_out.begin();
+  for (; x_out != state_out.end(); ++x, ++dxdt, ++x_out) {
+    *x_out = *x + *dxdt * dt;
+  }
+  return state_out;
+}
+
+}  // namespace operations
+
+template <typename State>
+class RungeKutta4 : public OdeSolver<RungeKutta4<State>> {};
+
+template <typename State>
+class Euler : public OdeSolver<Euler<State>> {
+ public:
+  Euler() {}
+
+  template <typename System, typename Time>
+  State update_impl(const System& system, const State& x, const Time t,
+                    const Time dt) {
+    return operations::cat(x, dt, system(x, t));
+  }
+};
+
+}  // namespace holon
+
+#endif  // HOLON_MATH_ODE_EULER_HPP_
---
+++ holon/corelib/math/ode_euler_test.cpp
@@ -0,0 +1,55 @@
+/* ode_euler - ordinary differential equation quadrature: Euler method
+ *
+ * Copyright (c) 2018 Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
+ *
+ * This file is part of the holon.
+ *
+ * The holon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The holon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the holon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "holon/corelib/math/ode_euler.hpp"
+
+#include <array>
+#include <iostream>
+
+#include "catch.hpp"
+
+namespace holon {
+namespace {
+
+using State = std::array<double, 2>;
+using Time = double;
+
+struct sys {
+  State operator()(const State& x, const Time t) const {
+    State dxdt;
+    std::cout << "sys start\n";
+    dxdt[0] = x[0] + 2.0 * x[1];
+    dxdt[1] = x[1];
+    std::cout << "sys end\n";
+    return dxdt;
+  }
+};
+
+TEST_CASE("Check ODE quadrapture with Euler method", "[ode][Euler]") {
+  Euler<State> solver;
+  State x = {{0, 1}};
+  double dt = 0.001;
+  x = solver.update(sys(), x, 0.0, dt);
+  CHECK(x[0] == Approx(0.0 + (0.0 + 2.0 * 1.0) * dt));
+  CHECK(x[1] == Approx(1.0 + (1.0) * dt));
+}
+
+}  // namespace
+}  // namespace holon

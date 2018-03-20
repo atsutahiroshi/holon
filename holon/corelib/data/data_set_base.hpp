d0eb97e596e5deabd8e4767b1476730fcf98f0ac
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-03-20 15:56:57 +0100

    feat(data): add DataSetBase class

---
+++ holon/corelib/data/data_set_base.hpp
@@ -0,0 +1,154 @@
+/* data_set_base - Base class for data set class
+ *
+ * Copyright (c) 2018 Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
+ *
+ * This file is part of the holon.
+ *
+ * The holon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The holon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the holon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef HOLON_DATA_DATA_SET_BASE_HPP_
+#define HOLON_DATA_DATA_SET_BASE_HPP_
+
+#include <memory>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include "holon/corelib/common/utility.hpp"
+
+namespace holon {
+
+struct RawDataBase {};
+
+template <typename RawData, typename... Args>
+std::shared_ptr<RawData> alloc_raw_data(Args&&... args) {
+  static_assert(std::is_base_of<RawDataBase, RawData>::value,
+                "RawData must inherit RawDataBase class.");
+  return std::make_shared<RawData>(std::forward<Args>(args)...);
+}
+
+template <class Derived, class... RawDataTypes>
+class DataSetBase {
+  static_assert(all_are_base_of<RawDataBase, RawDataTypes...>::value,
+                "RawDataTypes must inherit RawDataBase class.");
+  static_assert(sizeof...(RawDataTypes) > 0,
+                "DataSetBase must have at least one RawData class.");
+
+  using Self = DataSetBase<Derived, RawDataTypes...>;
+  using RawDataTuple = std::tuple<RawDataTypes...>;
+  using RawDataPtrTuple = std::tuple<std::shared_ptr<RawDataTypes>...>;
+  template <std::size_t N>
+  using RawData = typename std::tuple_element<N, RawDataTuple>::type;
+  template <std::size_t N>
+  using RawDataPtr = typename std::tuple_element<N, RawDataPtrTuple>::type;
+  static constexpr std::size_t raw_data_num = sizeof...(RawDataTypes);
+  static constexpr bool more_than_one = raw_data_num - 1;
+
+ public:
+  DataSetBase()
+      : m_data_ptr_tuple(std::make_tuple(alloc_raw_data<RawDataTypes>()...)) {}
+  explicit DataSetBase(const RawDataTypes&... args)
+      : m_data_ptr_tuple(
+            std::make_tuple(alloc_raw_data<RawDataTypes>(args)...)) {}
+  explicit DataSetBase(std::shared_ptr<RawDataTypes>... args)
+      : m_data_ptr_tuple(std::make_tuple(args...)) {}
+
+  const RawDataPtrTuple& get_data_ptr_tuple() const { return m_data_ptr_tuple; }
+
+  template <std::size_t I = 0>
+  RawDataPtr<I> get_ptr() {
+    return std::get<I>(m_data_ptr_tuple);
+  }
+  template <std::size_t I = 0>
+  const RawDataPtr<I>& get_ptr() const {
+    return std::get<I>(m_data_ptr_tuple);
+  }
+
+  template <std::size_t I = 0>
+  RawData<I>& get() {
+    return *std::get<I>(m_data_ptr_tuple);
+  }
+  template <std::size_t I = 0>
+  const RawData<I>& get() const {
+    return *std::get<I>(m_data_ptr_tuple);
+  }
+
+  typename std::conditional<more_than_one, Derived, RawData<0>>::type&
+  operator()() {
+    return this->call_op_impl(std::integral_constant<bool, more_than_one>());
+  }
+
+  void copy(const Self& other) { copy_impl<0>(other); }
+  void copy(const RawDataTypes... args) { copy_data_impl<0>(args...); }
+  void copy(const std::shared_ptr<RawDataTypes>... args) {
+    copy_data_impl<0>(args...);
+  }
+
+  void share_with(const Self& target) {
+    m_data_ptr_tuple = target.get_data_ptr_tuple();
+  }
+
+  Derived clone() {
+    Derived tmp;
+    tmp.copy(*this);
+    return tmp;
+  }
+
+ protected:
+  template <std::size_t I = 0>
+  void copy_index(const Self& other) {
+    this->get<I>() = other.get<I>();
+  }
+
+  template <std::size_t I = 0>
+  void copy_index(const RawData<I>& t_raw_data) {
+    this->get<I>() = t_raw_data;
+  }
+
+ private:
+  RawDataPtrTuple m_data_ptr_tuple;
+
+  Derived& call_op_impl(std::true_type) { return *static_cast<Derived*>(this); }
+  RawData<0>& call_op_impl(std::false_type) { return this->get<0>(); }
+
+  template <std::size_t I = 0>
+  typename std::enable_if<(I == raw_data_num), void>::type copy_impl(
+      const Self& /* other */) {}
+
+  template <std::size_t I = 0>
+  typename std::enable_if<(I < raw_data_num), void>::type copy_impl(
+      const Self& other) {
+    copy_index<I>(other);
+    copy_impl<I + 1>(other);
+  }
+
+  template <std::size_t I>
+  void copy_data_impl() {}
+
+  template <std::size_t I, typename T, typename... Types>
+  void copy_data_impl(const T& data, Types... args) {
+    copy_index<I>(data);
+    copy_data_impl<I + 1>(args...);
+  }
+
+  template <std::size_t I, typename T, typename... Types>
+  void copy_data_impl(std::shared_ptr<T> data_ptr, Types... args) {
+    copy_index<I>(*data_ptr);
+    copy_data_impl<I + 1>(args...);
+  }
+};
+
+}  // namespace holon
+
+#endif  // HOLON_DATA_DATA_SET_BASE_HPP_
---
+++ holon/corelib/data/data_set_base_test.cpp
@@ -0,0 +1,264 @@
+/* data_set_base - Base class for data set class
+ *
+ * Copyright (c) 2018 Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
+ *
+ * This file is part of the holon.
+ *
+ * The holon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The holon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the holon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "holon/corelib/data/data_set_base.hpp"
+
+#include <memory>
+
+#include "catch.hpp"
+#include "holon/test/util/fuzzer/fuzzer.hpp"
+
+namespace holon {
+namespace {
+
+TEST_CASE("Check a sample DataSet class is compilable", "[DataSetBase]") {
+  struct NotInheritRawDataBase {};
+  struct InheritRawDataBase : RawDataBase {};
+
+  // raise assertion
+  // class BadDataSet : DataSetBase<BadDataSet, NotInheritRawDataBase> {};
+  // no aserrtion
+  class GoodDataSet : DataSetBase<GoodDataSet, InheritRawDataBase> {};
+}
+
+struct SampleRawData : RawDataBase {
+  static constexpr double default_v = 1.0;
+  double v;
+  double x;
+
+  SampleRawData() : v(default_v), x(0.0) {}
+};
+
+struct MoreSampleRawData : RawDataBase {
+  static constexpr double default_in = 0.5;
+  double in;
+  double out;
+
+  MoreSampleRawData() : in(default_in), out(0.0) {}
+};
+
+class SampleDataSet : public DataSetBase<SampleDataSet, SampleRawData> {
+ public:
+  SampleDataSet() {}
+  explicit SampleDataSet(const SampleRawData& raw_data)
+      : DataSetBase<SampleDataSet, SampleRawData>(raw_data) {}
+  explicit SampleDataSet(std::shared_ptr<SampleRawData> raw_data)
+      : DataSetBase<SampleDataSet, SampleRawData>(raw_data) {}
+};
+
+class MoreSampleDataSet
+    : public DataSetBase<MoreSampleDataSet, SampleRawData, MoreSampleRawData,
+                         MoreSampleRawData> {
+ public:
+  MoreSampleDataSet() {}
+  MoreSampleDataSet(const SampleRawData& raw_data1,
+                    const MoreSampleRawData& raw_data2,
+                    const MoreSampleRawData& raw_data3)
+      : DataSetBase<MoreSampleDataSet, SampleRawData, MoreSampleRawData,
+                    MoreSampleRawData>(raw_data1, raw_data2, raw_data3) {}
+  MoreSampleDataSet(std::shared_ptr<SampleRawData> raw_data1,
+                    std::shared_ptr<MoreSampleRawData> raw_data2,
+                    std::shared_ptr<MoreSampleRawData> raw_data3)
+      : DataSetBase<MoreSampleDataSet, SampleRawData, MoreSampleRawData,
+                    MoreSampleRawData>(raw_data1, raw_data2, raw_data3) {}
+  SampleRawData& data1() { return get<0>(); }
+  MoreSampleRawData& data2() { return get<1>(); }
+  MoreSampleRawData& data3() { return get<2>(); }
+};
+
+TEST_CASE("Default constructor of DataSetBase", "[DataSetBase]") {
+  SECTION("Sample DataSet 1") {
+    SampleDataSet data;
+    REQUIRE(data.get_ptr<0>() != nullptr);
+    CHECK(data.get<0>().v == 1.0);
+  }
+  SECTION("Sample DataSet 2") {
+    MoreSampleDataSet data;
+    REQUIRE(data.get_ptr<0>() != nullptr);
+    REQUIRE(data.get_ptr<1>() != nullptr);
+    REQUIRE(data.get_ptr<2>() != nullptr);
+    CHECK(data.get<0>().v == 1.0);
+    CHECK(data.get<1>().in == 0.5);
+    CHECK(data.get<2>().in == 0.5);
+  }
+}
+
+TEST_CASE("Constructor which takes RawData instance should copy its data",
+          "[DataSetBase]") {
+  SECTION("Sample DataSet 1") {
+    SampleRawData raw_data;
+    raw_data.v = 3;
+    SampleDataSet data(raw_data);
+    REQUIRE(data.get_ptr<0>() != nullptr);
+    CHECK(data.get<0>().v == 3.0);
+    raw_data.v = 4;
+    CHECK(data.get<0>().v == 3.0);
+  }
+  SECTION("Sample DataSet 2") {
+    SampleRawData raw_data1;
+    MoreSampleRawData raw_data2, raw_data3;
+    raw_data1.v = 1.5;
+    raw_data2.in = 2.0;
+    raw_data3.in = 3.0;
+    MoreSampleDataSet data(raw_data1, raw_data2, raw_data3);
+    REQUIRE(data.get_ptr<0>() != nullptr);
+    REQUIRE(data.get_ptr<1>() != nullptr);
+    REQUIRE(data.get_ptr<2>() != nullptr);
+    CHECK(data.get<0>().v == 1.5);
+    CHECK(data.get<1>().in == 2.0);
+    CHECK(data.get<2>().in == 3.0);
+  }
+}
+
+TEST_CASE("Constructor which takes RawData pointer should share its data",
+          "[DataSetBase]") {
+  SECTION("Sample DataSet 1") {
+    auto raw_data_ptr = alloc_raw_data<SampleRawData>();
+    SampleDataSet data(raw_data_ptr);
+    REQUIRE(data.get_ptr<0>() == raw_data_ptr);
+  }
+  SECTION("Sample DataSet 2") {
+    auto raw_data_ptr1 = alloc_raw_data<SampleRawData>();
+    auto raw_data_ptr2 = alloc_raw_data<MoreSampleRawData>();
+    auto raw_data_ptr3 = alloc_raw_data<MoreSampleRawData>();
+    MoreSampleDataSet data(raw_data_ptr1, raw_data_ptr2, raw_data_ptr3);
+    REQUIRE(data.get_ptr<0>() == raw_data_ptr1);
+    REQUIRE(data.get_ptr<1>() == raw_data_ptr2);
+    REQUIRE(data.get_ptr<2>() == raw_data_ptr3);
+  }
+}
+
+TEST_CASE("Calling operator() in DataSet", "[DataSetBase]") {
+  SECTION("Sample DataSet 1") {
+    SampleDataSet data;
+    data().v = 3;
+    CHECK(data.get<0>().v == 3);
+  }
+  SECTION("Sample DataSet 2") {
+    MoreSampleDataSet data;
+    data().data1().v = 2;
+    CHECK(data.get<0>().v == 2);
+  }
+}
+
+TEST_CASE("Check behavior of copy constructor of DataSetBase",
+          "[DataSetBase]") {
+  SampleDataSet data1;
+  data1().v = Fuzzer().get();
+  SECTION("copy ctor should share its data pointer") {
+    SampleDataSet data2(data1);
+    REQUIRE(data1.get_ptr<0>() == data2.get_ptr<0>());
+    CHECK(data1().v == data2().v);
+  }
+}
+
+TEST_CASE("Check copy method in DataSetBase", "[DataSetBase]") {
+  SECTION("Sample DataSet 1") {
+    SampleDataSet data1;
+    data1().v = Fuzzer().get();
+    SampleDataSet data2;
+    REQUIRE(data2().v != data1().v);
+    data2.copy(data1);
+    CHECK(data2().v == data1().v);
+    CHECK(data2.get_ptr<0>() != data1.get_ptr<0>());
+  }
+  SECTION("Sample DataSet 2") {
+    Fuzzer fuzz;
+    MoreSampleDataSet data1;
+    data1.get<0>().v = fuzz();
+    data1.get<1>().in = fuzz();
+    data1.get<2>().in = fuzz();
+    MoreSampleDataSet data2;
+    REQUIRE(data2().get<0>().v != data1().get<0>().v);
+    REQUIRE(data2().get<1>().in != data1().get<1>().in);
+    REQUIRE(data2().get<2>().in != data1().get<2>().in);
+    data2.copy(data1);
+    CHECK(data2().get<0>().v == data1().get<0>().v);
+    CHECK(data2().get<1>().in == data1().get<1>().in);
+    CHECK(data2().get<2>().in == data1().get<2>().in);
+    CHECK(data2.get_ptr<0>() != data1.get_ptr<0>());
+    CHECK(data2.get_ptr<1>() != data1.get_ptr<1>());
+    CHECK(data2.get_ptr<2>() != data1.get_ptr<2>());
+  }
+}
+
+TEST_CASE("Check copy method in DataSetBase which takes RawData as arguments",
+          "[DataSetBase]") {
+  SECTION("Sample DataSet 1") {
+    Fuzzer fuzz;
+    SampleRawData raw_data;
+    raw_data.v = fuzz();
+    SampleDataSet data;
+    REQUIRE(data.get<0>().v != raw_data.v);
+    data.copy(raw_data);
+    REQUIRE(data.get_ptr<0>().get() != &raw_data);
+    CHECK(data.get<0>().v == raw_data.v);
+  }
+}
+
+TEST_CASE(
+    "Check copy method in DataSetBase which takes RawDataPtr as arguments",
+    "[DataSetBase]") {
+  SECTION("Sample DataSet 1") {
+    Fuzzer fuzz;
+    auto raw_data = alloc_raw_data<SampleRawData>();
+    raw_data->v = fuzz();
+    SampleDataSet data;
+    REQUIRE(data.get<0>().v != raw_data->v);
+    data.copy(raw_data);
+    REQUIRE(data.get_ptr<0>() != raw_data);
+    CHECK(data.get<0>().v == raw_data->v);
+  }
+}
+
+TEST_CASE("Check share_with method in DataSetBase", "[DataSetBase]") {
+  SECTION("Sample DataSet 1") {
+    SampleDataSet data1;
+    SampleDataSet data2;
+    REQUIRE(data2.get_ptr<0>() != data1.get_ptr<0>());
+    data2.share_with(data1);
+    CHECK(data2.get_ptr<0>() == data1.get_ptr<0>());
+  }
+  SECTION("Sample DataSet 2") {
+    MoreSampleDataSet data1;
+    MoreSampleDataSet data2;
+    REQUIRE(data2.get_ptr<0>() != data1.get_ptr<0>());
+    REQUIRE(data2.get_ptr<1>() != data1.get_ptr<1>());
+    REQUIRE(data2.get_ptr<2>() != data1.get_ptr<2>());
+    data2.share_with(data1);
+    CHECK(data2.get_ptr<0>() == data1.get_ptr<0>());
+    CHECK(data2.get_ptr<1>() == data1.get_ptr<1>());
+    CHECK(data2.get_ptr<2>() == data1.get_ptr<2>());
+  }
+}
+
+TEST_CASE("Check clone method in DataSetBase", "[DataSetBase]") {
+  SECTION("Sample DataSet 1") {
+    Fuzzer fuzz;
+    SampleDataSet data1;
+    data1().v = fuzz();
+    SampleDataSet data2(data1.clone());
+    REQUIRE(data2.get_ptr<0>() != data1.get_ptr<0>());
+    CHECK(data2().v == data1().v);
+  }
+}
+
+}  // namespace
+}  // namespace holon

da521225c2edca899b0b202bbc66ea6f6aedf7c7
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-05-12 10:16:13 +0200

    feat(humanoid): add BipedFootModelBuileder class

---
+++ holon2/corelib/humanoid/biped_foot_model/biped_foot_model_builder_test.cpp
@@ -0,0 +1,34 @@
+/* biped_foot_model_builder - Builder class of biped foot model
+ *
+ * Copyright (c) 2018 Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
+ *
+ * This file is part of holon.
+ *
+ * Holon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Holon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with holon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "holon2/corelib/humanoid/biped_foot_model/biped_foot_model_builder.hpp"
+#include "holon2/corelib/humanoid/biped_foot_model.hpp"
+
+#include "holon2/corelib/common/random.hpp"
+#include "third_party/catch/catch.hpp"
+
+namespace holon {
+namespace {
+
+TEST_CASE("biped_foot_model_builder: ",
+          "[BipedFootModel][BipedFootModelBuilder]") {}
+
+}  // namespace
+}  // namespace holon
